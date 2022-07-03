b812e2a25e113a485bc9b4e7cf548ab706d0f4f7
Aisha Abdi <aishahajisirad@gmail.com>
2022-07-03 19:54:29 +0100

    schema.sql

--- Main/Database/schema.sql
+++ Main/Database/schema.sql
@@ -1,4 +1,30 @@
 DROP DATABASE IF EXISTS employeetracker_db;
 CREATE employeetracker_db;
 
+USE employeetracker_db;
 
+CREATE TABLE department(
+    id INT AUTO_INCREMENT,
+    roles_id INT,
+    department_name VARCHAR(25) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+create TABLE roles (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(25),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employees (
+id INT AUTO_INCREMENT,
+first_name VARCHAR(25),
+last_name VARCHAR(25),
+roles_id INT,
+manager_id INT,
+PRIMARY KEY(id),
+FOREIGN KEY(manager_id) REFERENCES employees (id)
+);
